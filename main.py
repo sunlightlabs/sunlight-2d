fa3f1b1f658dd6cb71c36f3eeb61e59c841bfef1
jessykate <jessy.cowansharp@gmail.com>
2010-06-14 16:22:14 -0400

    basic functionality minus files atm

--- main.py
+++ main.py
@@ -4,11 +4,18 @@
 # ./main.py
 # and visit localhost:8888
 
+# fix files
+# todo add timestamp to each tag addition
+# add support for appending to a tag 
+
+
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 import urllib, urllib2
 import os, datetime
+from gridfs import GridFS
+
 try:
     import json
 except:
@@ -24,41 +31,124 @@ class MainHandler(tornado.web.RequestHandler):
         self.render('index.html')
 
 class UploadHandler(tornado.web.RequestHandler):
-    database = 'qrtaggr'
-    table = 'tags'
     def post(self):
+        context = {}
         # arguments is a dict of key:value pairs where each value is a
         # list (even if there is only one item). 
-        arguments = self.request.arguments
+        form = self.request.arguments
         print 'Arguments were:'
-        print arguments
-        # mongo speaks in dicts! so we can just pass the arguments
-        # right to the db, but in reality there's some processing
-        # here...
-        # 
-        # then:
+        print form
+        print 'files'
+        print self.request.files
+        if self.request.files.get('file', None):
+            print 'file body'
+            print type(self.request.files['file'][0]['body'])
+
+        # make sure the user submitted at least one of the fields:
+        if self.get_argument('body', "") == "" and not self.request.files.get('file', None):
+            print 'no content'
+            self.set_secure_cookie("message", "You must submit either a message or a file")
+            self.redirect('/')
+            return        
+
+        # build the new tag
+        newtag = {}
+        if self.get_argument('body', None):
+            newtag['body'] = self.get_argument('body')
+
+        if self.request.files.get('file', None):
+            newtag['filebody'] = self.request.files['file'][0]['body']
+            newtag['content_type'] = self.request.files['file'][0]['content_type']
+
+        # save it to the db and create a uri for the content
         try:
-            tag_id = self.db_save(arguments)
+            print 'newtag'
+            print newtag
+            tag_id = self.db_save(newtag)
+            context['tag_id'] = tag_id
         except BaseException, e:
-            tag_id = None
-        self.render('view.html', tag_id = tag_id)
+            print 'there was an error from mongo:'
+            print e
+            self.write("There was a problem")
+            return
 
-    def db_save(self, record):
-        db = pymongo.Connection()[self.database]
-        table = db[self.table]
+        # generate a qr code with the uri
+        uri = self.generate_uri(tag_id)
+        qrcode = create_qr(uri)
+        print qrcode
+        context['qr_url'] = qrcode
 
-        _id = table.insert(record, safe=True)
+        # send the qr code to printer
+
+        # show the user their newly created tag
+        self.redirect('/tag/%s' % str(tag_id))
+            
+    def generate_uri(self, tag_id):
+        uri = settings['root_url'].strip('/') + '/' + str(tag_id)
+        return uri
+
+    def db_save(self, thistag):
+        db = pymongo.Connection()[settings['database']]
+
+        # if there's a file, store it using gridFS and replace the
+        # file body in thistag, with reference to the file in
+        # gridFS (the file_id)
+#        if thistag.get('filebody', None):
+#            gfs = GridFS(db)
+#            file_id = gfs.put(thistag['filebody'], thistag['content_type'])
+#            print 'filebody'
+#            print thistag['filebody']
+#            del thistag['filebody']
+#            thistag['file'] = file_id
+
+        # create 'body' as a list; there might be more body
+        # elements added if multiple people edit the same tag
+        thistag['created'] = datetime.datetime.now()
+        tag = {'contents': [thistag,],
+               'last_updated' : thistag['created']
+               }        
+        table = db[settings['table']]
+        _id = table.insert(tag, safe=True)
         db.connection.disconnect()
         return _id
 
+def create_qr(uri):        
+    args = {
+        'chs' : '%dx%d' % (settings['qrx'], settings['qry']),
+        'chl' : uri,            
+        }
+    params = urllib.urlencode(args)
+    url = "http://chart.apis.google.com/chart?cht=qr&"+params
+    return url
+
 class ViewHandler(tornado.web.RequestHandler):
-    pass
+    def get(self, tag_id):        
+        print 'retrieving view info for tag_id %s' % tag_id
+        context = { 'qr_url' : create_qr(tag_id) }
+        table = pymongo.Connection()[settings['database']][settings['table']]
+        print table.count()
+        oid = pymongo.objectid.ObjectId(tag_id)
+        record = table.find_one({'_id': oid})
+        if record.get('file', None):
+            gfs = GridFS(db)
+            filebody = gfs.get(record.get('file'))  
+            record['file'] = filebody.read()
+        context['tag_items'] = record['contents']
+        self.render('view.html', context=context)        
+
+settings = {
+    'qrx' : 100,
+    'qry' : 100,
+    'root_url' : "http://localhost:8888",
+    'database' : 'qrtaggr',
+    'table' : 'tags',
+}
 
 application = tornado.web.Application([
         (r'/', MainHandler),
         (r'/upload', UploadHandler),
-        (r'/[\w]+', ViewHandler),
-        ])
+        (r'/tag/([\w]+)', ViewHandler),
+        ], cookie_secret="pYqy/FIEQKiXs/2XOlFMQ+GojmHkkUtnvxMxmifRxYA=", **settings)
 
 if __name__ == '__main__':
     http_server = tornado.httpserver.HTTPServer(application)

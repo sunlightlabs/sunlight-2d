e5d06c4e66703afdc03ba9dde28f532661edbf1a
klondike <kprojection@gmail.com>
2010-06-15 14:13:20 -0400

    Fixed local settings example

--- local_settings.example.py
+++ local_settings.example.py
@@ -1,9 +1,11 @@
+import os
+
 # copy and rename this to local_settings.py, and fill in the values 
 local_settings = {
   'root_url' : "http://example.com",
   'static_path': os.path.join(os.path.dirname(__file__), "static"),
   'database' : 'mydatabase',
-  'table' : 'mytab;e',
+  'table' : 'mytable',
   'S3_BUCKET' : "http://mybucket.amazon.com",
   'S3_KEY' : "s3 key here",
   'S3_SECRET' : "s3 secret here"

6647fda3b76410617016cf24cef98aa9f449c819
jessykate <jessy.cowansharp@gmail.com>
2010-06-16 11:37:18 -0400

    updated local settings exmaple file

--- local_settings.example.py
+++ local_settings.example.py
@@ -2,11 +2,13 @@ import os
 
 # copy and rename this to local_settings.py, and fill in the values 
 local_settings = {
-  'root_url' : "http://example.com",
-  'static_path': os.path.join(os.path.dirname(__file__), "static"),
-  'database' : 'mydatabase',
-  'table' : 'mytable',
-  'S3_BUCKET' : "http://mybucket.amazon.com",
-  'S3_KEY' : "s3 key here",
-  'S3_SECRET' : "s3 secret here"
+    'root_url' : "http://example.com",
+    'static_path': os.path.join(os.path.dirname(__file__), "static"),
+    # set printing value to 'enabled' to enable
+    'printing' : 'disabled',
+    'database' : 'mydatabase',
+    'table' : 'mytable',
+    'S3_BUCKET' : "http://mybucket.amazon.com",
+    'S3_KEY' : "s3 key here",
+    'S3_SECRET' : "s3 secret here"
 }
